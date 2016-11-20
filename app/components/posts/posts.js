be5434c62336573d0ad6f3d98d7749c5a5a3bd94
Sebastian <root@bio-hazard.de>
2016-11-20 13:15:38 +0100

    Post edited

--- app/components/posts/posts.js
+++ app/components/posts/posts.js
@@ -1,12 +1,12 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
 import {
     StyleSheet,
     View,
     Text,
-    ListView
+    ListView,
+    Navigator,
+    TouchableHighlight
 } from 'react-native';
-
-import {Actions} from 'react-native-router-flux';
 
 const styles = StyleSheet.create({
     container: {
@@ -19,18 +19,17 @@ const styles = StyleSheet.create({
 
 
 const stylesRow = StyleSheet.create({
-        container: {
-            flex: 1,
-            padding: 12,
-            flexDirection: 'column',
-            alignItems: 'flex-start',
-            borderBottomColor: 'black',
-            borderBottomWidth: 1
-        },
-        title: { marginLeft: 12, fontSize: 50},
-        content: {marginTop: 20, marginLeft: 12, fontSize: 16},
-    })
-    ;
+    container: {
+        flex: 1,
+        padding: 12,
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        borderBottomColor: 'black',
+        borderBottomWidth: 1
+    },
+    title: {marginLeft: 12, fontSize: 50},
+    content: {marginTop: 20, marginLeft: 12, fontSize: 16},
+});
 
 const Row = (props) => (
     <View style={stylesRow.container}>
@@ -45,20 +44,59 @@ class Posts extends Component {
         const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
         this.state = {
             dataSource: ds.cloneWithRows([
-                {title: 'Stars are spinning around your head?', content: 'Maybe the earth started to rotate in a much faster way, keep calm. It does not take long till every the earth as we know it wil change to something very cruel.'},
-                {title: 'React native, the new hotness', content: 'Some one needs to explain what is hot about this stuff'}
+                {
+                    title: 'Stars are spinning around your head?',
+                    content: 'Maybe the earth started to rotate in a much faster way, keep calm. It does not take long till every the earth as we know it wil change to something very cruel.'
+                },
+                {
+                    title: 'React native, the new hotness',
+                    content: 'Some one needs to explain what is hot about this stuff'
+                }
             ]),
         };
     }
 
     render() {
         return (
-            <ListView
-                dataSource={this.state.dataSource}
-                renderRow={(data) => <Row {...data} />}
-            />
+            <View>
+                <ListView
+                    dataSource={this.state.dataSource}
+                    renderRow={(data) => <Row {...data} />}
+                />
+            </View>
         );
     }
 }
 
-export default Posts;
+export default class PostNavigator extends Component {
+    render() {
+        return (
+            <Navigator
+                initialRoute={{title: 'My Initial Scene', index: 0}}
+                renderScene={(route, navigator) =>
+                    <Posts
+                        title={route.title}
+
+                        // Function to call when a new scene should be displayed
+                        onForward={ () => {
+                            const nextIndex = route.index + 1;
+                            navigator.push({
+                                title: 'Scene ' + nextIndex,
+                                index: nextIndex,
+                            });
+                        }}
+
+                        // Function to call to go back to the previous scene
+                        onBack={() => {
+                            if (route.index > 0) {
+                                navigator.pop();
+                            }
+                        }}
+                    />
+                }
+            />
+        )
+    }
+}
+
+// export default Posts;

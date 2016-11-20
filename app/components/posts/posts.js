7eff599efaaaa5c51daf2d09a130f6a1ec5d15f8
Sebastian <root@bio-hazard.de>
2016-11-20 14:59:40 +0100

    Adding refresh

--- app/components/posts/posts.js
+++ app/components/posts/posts.js
@@ -5,7 +5,8 @@ import {
     Text,
     ListView,
     Navigator,
-    TouchableHighlight
+    TouchableHighlight,
+    RefreshControl
 } from 'react-native';
 
 
@@ -21,11 +22,12 @@ const Row = (props) => (
 );
 
 class Posts extends Component {
+
     constructor() {
         super();
-        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
         this.state = {
-            dataSource: ds.cloneWithRows([
+            dataSource: this.ds.cloneWithRows([
                 {
                     title: 'Stars are spinning around your head?',
                     content: 'Maybe the earth started to rotate in a much faster way, keep calm. It does not take long till every the earth as we know it wil change to something very cruel.'
@@ -35,15 +37,44 @@ class Posts extends Component {
                     content: 'Some one needs to explain what is hot about this stuff'
                 }
             ]),
+            refreshing: false,
         };
+    }
+
+    fetchData() {
+        return fetch('http://185.14.186.74:3000/posts')
+            .then((response) => response.json())
+            .then((responseJson) => {
+                this.setState({
+                    dataSource: this.ds.cloneWithRows(responseJson)
+                });
+
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
+    _onRefresh() {
+        this.setState({refreshing: true});
+        this.fetchData().then(() => {
+            this.setState({refreshing: false});
+        });
     }
 
     render() {
         return (
             <View>
                 <ListView
+                    enableEmptySections={true}
                     dataSource={this.state.dataSource}
                     renderRow={(data) => <Row {...data} />}
+                    refreshControl={
+                        <RefreshControl
+                            refreshing={this.state.refreshing}
+                            onRefresh={this._onRefresh.bind(this)}
+                        />
+                    }
                 />
             </View>
         );
